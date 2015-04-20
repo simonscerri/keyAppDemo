5be4e87c69de14a80c6e9671fff5bf6ce840bbf5
Simon Scerri-Taylor <sim.scerri@gmail.com>
2015-04-20 15:12:16 +0100

    KeyApp Device Demo
    version 1.1 - Supports device id storage

---
+++ includes/db_connection.php
@@ -0,0 +1,17 @@
+<?php //create a connection to the database
+/*
+	require("constants.php");
+    $connection1 = new mysqli(DB_SERVER1, DB_USER1, DB_PASSWORD1, DB_NAME1);
+    if ($connection1->connect_errno) {
+        die("Database connection failed:" . mysql_error());
+    }
+*/
+	$link = mysql_connect('mysql01.internal.m2mconnected.com', 'keyApp_read', 'Tmwyso&ge9v4bie'); 
+	if (!$link) { 
+		die('Could not connect: ' . mysql_error()); 
+	} 
+	
+	mysql_select_db(arqtestm2m); 
+*/
+?> 
+    
